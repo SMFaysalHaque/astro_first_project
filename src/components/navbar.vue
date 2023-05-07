93bc5ceff51c3941dc5f2131938488452b52e1a2
SMFaysalHaque <smfaysalhaque007@gmail.com>
2023-05-07 19:24:49 +0600

    dashboard and persons info updated

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,30 +2,52 @@
 
 <!-- navigation bar stat -->
 
-<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
-  </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+<!-- <nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="https://flowbite.com/" class="flex items-center">
+      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+  </a>
+  <div class="flex md:order-2">
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+      <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
   </div>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Docs
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Examples
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-        Blog
-      </a>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+      </li>
+      <li>
+        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav> -->
+
+<nav class="">
+  <div class="flex flex-wrap items-center justify-between border border-pink-600 w-full">
+    <div class="logo">
+      <p class="font text-3xl font-bold">IMS</p>
     </div>
-    <div>
-      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+    <div class="list-topic">
+      <ul class="flex">
+        <li class="px-5">All Units</li>
+        <li class="px-5">Persons</li>
+        <li class="px-5">Unit Types</li>
+      </ul>
+    </div>
+    <div class="">
+      <input class="border rounded-lg contrast-more:border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 p-2" type="search" name="" id="" placeholder="Search Employee Name">
     </div>
   </div>
 </nav>
@@ -35,6 +57,7 @@
 </template>
 
 <script>
+
 export default {
     
 }
---
+++ src/layouts/person.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <div class="headline-searchBar flex items-center justify-between border border-purple-600">
+        <p class="font-bold text-lg">Persons</p>
+        <input class="border rounded-lg contrast-more:border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 p-2" type="search" name="" id="" placeholder="Search Employee Name">
+    </div>
+    
+    <div class="grid grid-cols-3 border border-cyan-700">
+        <div class="border border-cyan-700 h-auto">
+            <p>Name:</p>
+            <p>Mobile:</p>
+            <p>E-mail:</p>
+        </div>
+        <div class="border border-cyan-700">
+            <table class="mx-5 my-5">
+                <thead>
+                    <tr>
+                        <th class="border border-slate-300 bg-slate-100 w-32 py-2 text-center">Status</th>
+                        <th class="border border-slate-300 bg-slate-100 w-32 py-2 text-center">Serial</th>
+                        <th class="border border-slate-300 bg-slate-100 w-32 py-2 text-center">Type</th>
+                        <th class="border border-slate-300 bg-slate-100 w-32 py-2 text-center">Name</th>
+                        <th class="border border-slate-300 bg-slate-100 w-32 py-2 text-center">Assign To</th>
+                        <th class="border border-slate-300 bg-slate-100 w-32 py-2 text-center">From</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="border border-slate-300 text-center py-3">Indiana</td>
+                        <td class="border border-slate-300 text-center py-3">Indianapolis</td>
+                        <td class="border border-slate-300 text-center py-3">Ohio</td>
+                        <td class="border border-slate-300 text-center py-3">Columbus</td>
+                        <td class="border border-slate-300 text-center py-3">Michigan</td>
+                        <td class="border border-slate-300 text-center py-3">Detroit</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-slate-300 text-center py-3">Indiana</td>
+                        <td class="border border-slate-300 text-center py-3">Indianapolis</td>
+                        <td class="border border-slate-300 text-center py-3">Ohio</td>
+                        <td class="border border-slate-300 text-center py-3">Columbus</td>
+                        <td class="border border-slate-300 text-center py-3">Michigan</td>
+                        <td class="border border-slate-300 text-center py-3">Detroit</td>
+                    </tr>
+                    <tr>
+                        <td class="border border-slate-300 text-center py-3">Indiana</td>
+                        <td class="border border-slate-300 text-center py-3">Indianapolis</td>
+                        <td class="border border-slate-300 text-center py-3">Ohio</td>
+                        <td class="border border-slate-300 text-center py-3">Columbus</td>
+                        <td class="border border-slate-300 text-center py-3">Michigan</td>
+                        <td class="border border-slate-300 text-center py-3">Detroit</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="border border-cyan-700">
+            <button class="border rounded-lg px-10 py-3 me-2">Assign Product</button>
+            <button class="border rounded-lg px-10 py-3">Details</button>
+        </div>
+    </div>
+    </div>
+
+</template>
